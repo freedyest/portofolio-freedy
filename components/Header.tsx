c086ae5e16e7c1a187ab38441d6a4bcf5f0d1f69
freedyest <freedyjob@gmail.com>
2025-10-27 19:17:02 +0700

    header menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,15 @@
 export default function Header() {
   return (
-    <header className="bg-gray-900 text-white py-6">
-      <div className="container mx-auto px-4 text-center">
+    <section className="bg-gray-950 text-white py-6 px-6">
+      <div className="container flex mx-auto px-4 text-center justify-between items-center">
         <h1 className="text-3xl font-bold">Freedy</h1>
-        <p className="text-gray-400">Web Developer</p>
+        <div className="gap-3 px-2 flex font-semibold">
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Portofolio</a>
+          <a href="">Contact</a>
+        </div>
       </div>
-    </header>
+    </section>
   );
 }
