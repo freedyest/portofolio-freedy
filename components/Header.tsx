b0a11f46c9d4281a1fc407073f940ba2a567d814
freedyest <freedyjob@gmail.com>
2025-10-27 18:59:33 +0700

    init header

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="bg-gray-900 text-white py-6">
+      <div className="container mx-auto px-4 text-center">
+        <h1 className="text-3xl font-bold">Freedy</h1>
+        <p className="text-gray-400">Web Developer</p>
+      </div>
+    </header>
+  );
+}
