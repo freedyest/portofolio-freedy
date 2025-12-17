4522a6ed7014fc54833606eb41ad4059d6cbb9a2
freedyest <freedyjob@gmail.com>
2025-12-17 23:31:19 +0700

    adjust justify

--- components/Project.tsx
+++ components/Project.tsx
@@ -59,7 +59,7 @@ export default function Project() {
             className="block h-full"
           >
             <motion.div
-              className="group shadow-lg flex flex-col justify-between h-full p-6 bg-warmyellow dark:bg-slate-200 rounded-xl 
+              className="group shadow-lg flex flex-col justify-start h-full p-6 bg-warmyellow dark:bg-slate-200 rounded-xl 
                    hover:shadow-2xl hover:shadow-yellow-500/40 hover:border-2 
                    hover:border-primary cursor-pointer overflow-hidden"
               initial={{ opacity: 0, y: 30 }}
